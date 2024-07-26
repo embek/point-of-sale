09ea87dbe8c23bbc9865bf165bd300db57754c3a
Adzka <adzka2008@gmail.com>
2024-07-26 16:00:26 +0700

    lengkapi views purchases

--- routes/purchases.js
+++ routes/purchases.js
@@ -6,4 +6,11 @@ router.get('/', (req, res) => {
     res.render('purchases/list', { operator: req.session.userid.name })
 })
 
+router.get('/add', (req, res) => {
+    res.render('purchases/add', { operator: req.session.userid.name })
+})
+
+router.get('/edit/:invoice', (req, res) => {
+    res.render('purchases/edit', { operator: req.session.userid.name })
+})
 module.exports = router;
